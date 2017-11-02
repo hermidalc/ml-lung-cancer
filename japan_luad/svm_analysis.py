4949fe067a50e16de39579a2f1de748577953c86
Leandro Hermida <hermidal@cs.umd.edu>
2017-11-02 11:44:52 -0400

    Additional work on SVM analysis

--- japan_luad/svm_analysis.py
+++ japan_luad/svm_analysis.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
@@ -13,12 +14,17 @@ from sklearn import svm
 style.use("ggplot")
 base = importr("base")
 base.load("eset_gex.Rda")
-eset_gex = robjects.globalenv["eset.gex"]
-base.source("select_exp_features.R")
-r_select_exp_features = robjects.globalenv["selectExpressionFeatures"]
-for i in range(0, 10):
-    table = r_select_exp_features(eset_gex)
-    print(base.summary(table))
-    print(i)
-
-svc = svm.SVC(kernel="linear")
+eset = robjects.globalenv["eset.gex"]
+base.source("functions.R")
+r_rand_perm_sample_nums = robjects.globalenv["randPermSampleNums"]
+r_filter_eset_fs = robjects.globalenv["filterEsetFs"]
+r_select_exp_features = robjects.globalenv["selectExpFeatures"]
+relapse_fs_percent = .15
+for i in range(0, 1):
+    relapse_sample_nums = r_rand_perm_sample_nums(eset, 1)
+    norelapse_sample_nums = r_rand_perm_sample_nums(eset, 0)
+    num_fs_relapse_samples = math.ceil(len(relapse_sample_nums) * relapse_fs_percent)
+    num_fs_norelapse_samples = len(norelapse_sample_nums) - len(relapse_sample_nums) + num_fs_relapse_samples
+    eset_fs = r_filter_eset_fs(eset, relapse_sample_nums[:num_fs_relapse_samples], norelapse_sample_nums[:num_fs_norelapse_samples])
+    features = r_select_exp_features(eset_fs)
+    
