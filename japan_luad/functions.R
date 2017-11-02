a7283bedaf5513b793baae3d952f45008ea403b0
Leandro Hermida <hermidal@cs.umd.edu>
2017-11-02 11:44:19 -0400

    Change R file name and refactored function implementation

---
+++ japan_luad/functions.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env R
+
+suppressPackageStartupMessages(library("Biobase"))
+suppressPackageStartupMessages(library("limma"))
+
+randPermSampleNums <- function(eset, is.relapse) {
+    return(sample(which(eset$Relapse == is.relapse)))
+}
+
+filterEsetFs <- function(eset, relapse.samples, norelapse.samples) {
+    return(eset[,sort(c(relapse.samples, norelapse.samples))])
+}
+
+# selectExpFeatures <- function(eset, min.p.value = 0.05, min.lfc = 1.5, max.num.features = 50) {
+selectExpFeatures <- function(eset, max.num.features = 50) {
+    design <- model.matrix(~0 + factor(pData(eset)$Relapse))
+    colnames(design) <- c("NoRelapse", "Relapse")
+    fit <- lmFit(eset, design)
+    contrast.matrix <- makeContrasts(RelapseVsNoRelapse=Relapse-NoRelapse, levels=design)
+    fit.contrasts <- contrasts.fit(fit, contrast.matrix)
+    fit.b <- eBayes(fit.contrasts)
+    # results <- decideTests(fit.b, method="global")
+    # summary(results)
+    # return(topTable(fit.b, number=max.num.features, p.value=min.p.value, lfc=min.lfc, adjust.method="BH", sort.by="logFC"))
+    return(topTable(fit.b, number=max.num.features, adjust.method="BH", sort.by="logFC"))
+}
