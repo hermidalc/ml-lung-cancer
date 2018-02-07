770e12c1a13ea76e7e2defbcd4e8a0903db737e6
Leandro Hermida <hermidal@cs.umd.edu>
2018-02-07 12:56:58 -0500

    Updates to R config

--- luad/config.R
+++ luad/config.R
@@ -1,17 +1,26 @@
 #!/usr/bin/env/R
 
-eset_tr_strs = c(
+eset_single_tr_names <- c(
+    "eset_gex_gse31210"
+)
+eset_single_te_names <- c(
+    "eset_gex_gse8894",
+    "eset_gex_gse30219",
+    "eset_gex_gse37745",
+    "eset_gex_gse50081"
+)
+eset_merged_tr_names <- c(
     "eset_gex_gse31210_gse8894_gse30219_gse37745",
     "eset_gex_gse31210_gse8894_gse30219_gse50081",
     "eset_gex_gse31210_gse8894_gse37745_gse50081",
     "eset_gex_gse31210_gse30219_gse37745_gse50081",
     "eset_gex_gse8894_gse30219_gse37745_gse50081"
 )
-eset_te_strs = c(
+eset_merged_te_names <- c(
     "eset_gex_gse50081",
     "eset_gex_gse37745",
     "eset_gex_gse30219",
     "eset_gex_gse8894",
     "eset_gex_gse31210"
 )
-stica_alphas = c( 0, 0.25, 0.5, 0.75, 1 )
+stica_alphas <- c( 0, 0.25, 0.5, 0.75, 1 )
