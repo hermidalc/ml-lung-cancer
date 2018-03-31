800810def7a7c8da5f7ba79570271286b8b78d8a
Leandro Hermida <hermidal@cs.umd.edu>
2018-03-31 12:36:42 -0400

    Variable name updates

--- luad/process_raw_data.R
+++ luad/process_raw_data.R
@@ -27,20 +27,20 @@ if ("gcrma" %in% cmd_args[3:length(cmd_args)]) {
     affinities <- compute.affinities(cdfname, verbose=TRUE)
 }
 for (col in 1:ncol(dataset_tr_name_combos)) {
-    cel_files_tr <- c()
+    cel_tr_files <- c()
     for (dataset_tr_name in dataset_tr_name_combos[,col]) {
-        cel_path_tr <- paste0("data/raw/", dataset_tr_name)
-        if (dir.exists(cel_path_tr)) {
-            cel_files_tr <- append(cel_files_tr, list.files(path=cel_path_tr, full.names=TRUE, pattern="\\.CEL$"))
+        cel_tr_dir <- paste0("data/raw/", dataset_tr_name)
+        if (dir.exists(cel_tr_dir)) {
+            cel_tr_files <- append(cel_tr_files, list.files(path=cel_tr_dir, full.names=TRUE, pattern="\\.CEL$"))
         }
         else {
-            cel_files_tr <- NULL
+            cel_tr_files <- NULL
             break
         }
     }
-    if (is.null(cel_files_tr)) next
+    if (is.null(cel_tr_files)) next
     print(paste0(c("Creating AffyBatch:", dataset_tr_name_combos[,col]), collapse=" "))
-    affybatch_tr <- ReadAffy(filenames=cel_files_tr, cdfname=cdfname, verbose=TRUE)
+    affybatch_tr <- ReadAffy(filenames=cel_tr_files, cdfname=cdfname, verbose=TRUE)
     eset_tr_name <- paste0(c("eset", dataset_tr_name_combos[,col]), collapse="_")
     for (norm_type in cmd_args[3:length(cmd_args)]) {
         eset_tr_norm_name <- paste0(c(eset_tr_name, norm_type, "tr"), collapse="_")
@@ -72,13 +72,13 @@ for (col in 1:ncol(dataset_tr_name_combos)) {
         assign(eset_tr_norm_obj_name, norm_obj)
         save(list=eset_tr_norm_obj_name, file=paste0("data/", eset_tr_norm_obj_name, ".Rda"))
         for (dataset_te_name in setdiff(dataset_names, dataset_tr_name_combos[,col])) {
-            cel_path_te <- paste0("data/raw/", dataset_te_name)
-            if (!dir.exists(cel_path_te)) next
-            cel_files_te <- list.files(path=cel_path_te, full.names=TRUE, pattern="\\.CEL$")
+            cel_te_dir <- paste0("data/raw/", dataset_te_name)
+            if (!dir.exists(cel_te_dir)) next
+            cel_te_files <- list.files(path=cel_te_dir, full.names=TRUE, pattern="\\.CEL$")
             eset_te_name <- paste0("eset_", dataset_te_name)
             eset_te_norm_name <- paste0(eset_tr_norm_name, "_", dataset_te_name, "_te")
             print(paste("Creating AffyBatch:", dataset_te_name))
-            affybatch_te <- ReadAffy(filenames=cel_files_te, cdfname=cdfname, verbose=TRUE)
+            affybatch_te <- ReadAffy(filenames=cel_te_files, cdfname=cdfname, verbose=TRUE)
             print(paste("Creating:", eset_te_norm_name))
             if (norm_type == "gcrma") {
                 xnorm_te <- gcrmaaddon(norm_obj, affybatch_te, affinities)
