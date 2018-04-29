8ebc3c81ec71b71b3363cfe74afc56428e899f5c
Leandro Hermida <hermidal@cs.umd.edu>
2018-04-29 01:37:36 -0400

    Refactor for argparse

--- nsclc/process_raw_data_custom.R
+++ nsclc/process_raw_data_custom.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+suppressPackageStartupMessages(library("argparse"))
 suppressPackageStartupMessages(library("preprocessCore"))
 suppressPackageStartupMessages(library("affy"))
 suppressPackageStartupMessages(suppressWarnings(library("hgu133plus2.db")))
@@ -7,65 +8,77 @@ suppressPackageStartupMessages(suppressWarnings(library("hgu133plus2hsentrezg.db
 suppressPackageStartupMessages(library("annotate"))
 source("config.R")
 
-cmd_args <- commandArgs(trailingOnly=TRUE)
-id_type <- cmd_args[1]
-if (id_type == "gene") {
+parser <- ArgumentParser()
+parser$add_argument("--norm-meth", type="character", nargs="+", help="preprocessing/normalization method")
+parser$add_argument("--id-type", type="character", nargs="+", help="dataset id type")
+args <- parser$parse_args()
+num_tr_combo <- as.integer(args$num_tr_combo)
+if (!is.null(args$norm_meth)) {
+    arg_norm_methods <- norm_methods[norm_methods %in% args$norm_meth]
+}
+if (!is.null(args$id_type)) {
+    id_types <- id_types[id_types %in% args$id_type]
+}
+if ("gene" %in% id_types) {
     cdfname <- "hgu133plus2hsentrezg"
 } else {
     cdfname <- "hgu133plus2"
 }
-if (id_type == "gene" && "mas5" %in% cmd_args[2:length(cmd_args)]) {
+if (id_type == "gene" & "mas5" %in% cmd_args[2:length(cmd_args)]) {
     eset_gse67639_name <- "eset_gse67639_mas5_gene"
     cat("Loading:", eset_gse67639_name, "\n")
     load(paste0("data/", eset_gse67639_name, ".Rda"))
 }
 for (dataset_name in dataset_names) {
     if (!dir.exists(paste0("data/raw/", dataset_name))) next
-    for (norm_type in cmd_args[2:length(cmd_args)]) {
-        # load a reference eset
-        for (ref_norm_type in norm_types) {
-            ref_suffixes <- c(ref_norm_type)
-            if (!is.na(id_type) & id_type != "gene") ref_suffixes <- c(ref_suffixes, id_type)
-            eset_ref_name <- paste0(c("eset", dataset_name, ref_suffixes), collapse="_")
-            eset_ref_file <- paste0("data/", eset_ref_name, ".Rda")
-            if (file.exists(eset_ref_file) & !exists(eset_ref_name)) {
-                cat("Loading:", eset_ref_name, "\n")
-                load(eset_ref_file)
-                break
+    for (norm_meth in arg_norm_methods) {
+        for (id_type in id_types) {
+            # load a reference eset
+            for (ref_norm_meth in norm_methods) {
+                ref_suffixes <- c(ref_norm_meth)
+                if (id_type != "gene") ref_suffixes <- c(ref_suffixes, id_type)
+                eset_ref_name <- paste0(c("eset", dataset_name, ref_suffixes), collapse="_")
+                eset_ref_file <- paste0("data/", eset_ref_name, ".Rda")
+                if (file.exists(eset_ref_file) & !exists(eset_ref_name)) {
+                    cat("Loading:", eset_ref_name, "\n")
+                    load(eset_ref_file)
+                    break
+                }
             }
-        }
-        suffixes <- c(norm_type)
-        if (!is.na(id_type) & id_type != "none") suffixes <- c(suffixes, id_type)
-        eset_norm_name <- paste0(c("eset", dataset_name, suffixes), collapse="_")
-        cat("Creating:", eset_norm_name, "\n")
-        affybatch <- ReadAffy(
-            celfile.path=paste0("data/raw/", dataset_name), cdfname=cdfname, verbose=TRUE
-        )
-        if (norm_type == "rma") {
-            exprs <- exprs(rma(affybatch))
-        }
-        else if (norm_type == "mas5") {
-            exprs <- normalize.quantiles(log2(exprs(mas5(affybatch))))
-        }
-        colnames(exprs) <- sub("\\.CEL$", "", colnames(exprs))
-        if (id_type == "gene") {
-            if (exists(eset_gse67639_name)) {
-                
-            }
-            eset_norm <- ExpressionSet(
-                assayData=exprs,
-                phenoData=phenoData(get(eset_ref_name)),
-                featureData=AnnotatedDataFrame(data.frame(
-                    Symbol=getSYMBOL(rownames(exprs), paste0(cdfname, ".db"))
-                )),
-                annotation=cdfname
+            suffixes <- c(norm_meth)
+            if (id_type != "none") suffixes <- c(suffixes, id_type)
+            eset_norm_name <- paste0(c("eset", dataset_name, suffixes), collapse="_")
+            cat("Creating:", eset_norm_name, "\n")
+            affybatch <- ReadAffy(
+                celfile.path=paste0("data/raw/", dataset_name), cdfname=cdfname, verbose=TRUE
             )
+            if (norm_meth == "rma") {
+                exprs <- exprs(rma(affybatch))
+            }
+            else if (norm_meth == "mas5") {
+                exprs <- normalize.quantiles(log2(exprs(mas5(affybatch))))
+            }
+            colnames(exprs) <- sub("\\.CEL$", "", colnames(exprs))
+            if (id_type == "gene") {
+                if (exists(eset_gse67639_name)) {
+                    
+                }
+                eset_norm <- ExpressionSet(
+                    assayData=exprs,
+                    phenoData=phenoData(get(eset_ref_name)),
+                    featureData=AnnotatedDataFrame(data.frame(
+                        Symbol=getSYMBOL(rownames(exprs), paste0(cdfname, ".db"))
+                    )),
+                    annotation=cdfname
+                )
+            }
+            else {
+                eset_norm <- get(eset_ref_name)
+                exprs(eset_norm) <- exprs
+            }
+            assign(eset_norm_name, eset_norm)
+            save(list=eset_norm_name, file=paste0("data/", eset_norm_name, ".Rda"))
+            remove(list=c(eset_norm_name))
         }
-        else {
-            eset_norm <- get(eset_ref_name)
-            exprs(eset_norm) <- exprs
-        }
-        assign(eset_norm_name, eset_norm)
-        save(list=eset_norm_name, file=paste0("data/", eset_norm_name, ".Rda"))
     }
 }
