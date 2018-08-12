30b3dd146f7c79d5adb9a1ed62175b57064b2603
Leandro Hermida <hermidal@cs.umd.edu>
2018-08-12 16:32:11 -0400

    Added print statement

--- met/preprocess_data.R
+++ met/preprocess_data.R
@@ -9,6 +9,7 @@ dataset_tr_name <- "lhc_nsclc_met"
 suffixes <- c()
 df_x_tr_name <- paste0(c("df", "X", dataset_tr_name, suffixes), collapse="_")
 df_p_tr_name <- paste0(c("df", "p", dataset_tr_name, suffixes), collapse="_")
+cat("Creating:", df_x_tr_name, "+", df_p_tr_name, "\n")
 df_x_tr <- read.delim(paste0("data/", dataset_tr_name, "_data.txt"), row.names=1)
 df_p_tr <- read.delim(paste0("data/", dataset_tr_name, "_meta.txt"), row.names=2)
 assign(df_x_tr_name, df_x_tr)
